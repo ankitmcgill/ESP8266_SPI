9ac51f04f56150642ce5ec3ac1ade15cc0a65475
Ankit Bhatnagar <ankit.bhatnagarindia@gmail.com>
2016-10-26 23:41:26 +0530

    bit banged spi skeleton initial commit

--- ESP8266_SPI.c
+++ ESP8266_SPI.c
@@ -1,5 +1,7 @@
 /*************************************************
 * ESP8266 HARDWARE SPI (HSPI) LIBRARY
+* NOT USING HARDWARE HSPI
+* BIT BANGING
 *
 * OCTOBER 26 2016
 * ANKIT BHATNAGAR
@@ -7,3 +9,28 @@
 * ***********************************************/
 
 #include "ESP8266_SPI.h"
+
+void ESP8266_SPI_init_pins(void)
+{
+
+
+}
+
+void ESP8266_SPI_set_params(void)
+{
+
+
+
+}
+
+void ESP8266_SPI_send(uint8_t address, uint8_t data)
+{
+
+
+}
+
+void ESP8266_SPI_get(uint8_t* data)
+{
+
+
+}
--- ESP8266_SPI.h
+++ ESP8266_SPI.h
@@ -1,5 +1,7 @@
 /*************************************************
 * ESP8266 HARDWARE SPI (HSPI) LIBRARY
+* NOT USING HARDWARE HSPI
+* BIT BANGING
 *
 * OCTOBER 26 2016
 * ANKIT BHATNAGAR
@@ -8,5 +10,19 @@
 
 #ifndef _ESP8266_SPI_H_
 #define _ESP8266_SPI_H_
+
+
+
+#include "ESP8266_SPI.h"
+#include "spi_register.h"
+#include "user_interface.h"
+#include <ets_sys.h>
+#include "gpio.h"
+#include "spi.h"
+
+void ESP8266_SPI_init_pins(void);
+void ESP8266_SPI_set_params(void);
+void ESP8266_SPI_send(uint32_t address, uint32_t data);
+void ESP8266_SPI_get(uint8_t* data);
 
 #endif

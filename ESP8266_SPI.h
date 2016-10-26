5600ff6425c0110ff17bad0dca6a07f9673ff11f
Ankit Bhatnagar <ankit.bhatnagarindia@gmail.com>
2016-10-26 23:36:49 +0530

    hardware spi implementation initial commit

--- ESP8266_SPI.h
+++ ESP8266_SPI.h
@@ -9,4 +9,18 @@
 #ifndef _ESP8266_SPI_H_
 #define _ESP8266_SPI_H_
 
+
+
+#include "ESP8266_SPI.h"
+#include "spi_register.h"
+#include "user_interface.h"
+#include <ets_sys.h>
+#include "gpio.h"
+#include "spi.h"
+
+void ESP8266_SPI_init_pins(void);
+void ESP8266_SPI_set_params(void);
+void ESP8266_SPI_send(uint32_t address, uint32_t data);
+void ESP8266_SPI_get(uint8_t* data);
+
 #endif

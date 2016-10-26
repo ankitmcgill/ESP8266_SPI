aa67350821ab2b8184e03a15b56f1ed112cdb895
Ankit Bhatnagar <ankit.bhatnagarindia@gmail.com>
2016-10-26 11:12:42 +0530

    Initial file commit

---
+++ ESP8266_SPI.c
@@ -0,0 +1,9 @@
+/*************************************************
+* ESP8266 HARDWARE SPI (HSPI) LIBRARY
+*
+* OCTOBER 26 2016
+* ANKIT BHATNAGAR
+* ANKIT.BHATNAGARINDIA@GMAIL.COM
+* ***********************************************/
+
+#include "ESP8266_SPI.h"
---
+++ ESP8266_SPI.h
@@ -0,0 +1,12 @@
+/*************************************************
+* ESP8266 HARDWARE SPI (HSPI) LIBRARY
+*
+* OCTOBER 26 2016
+* ANKIT BHATNAGAR
+* ANKIT.BHATNAGARINDIA@GMAIL.COM
+* ***********************************************/
+
+#ifndef _ESP8266_SPI_H_
+#define _ESP8266_SPI_H_
+
+#endif

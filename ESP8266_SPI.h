b0620847b5e944fe0abc58bd7fd6d71b7d0fe5f7
Ankit Bhatnagar <ankit.bhatnagarindia@gmail.com>
2016-10-31 20:20:23 +0530

    Fixed hardware SPI for ESP8266. working copy th address + data support

--- ESP8266_SPI.h
+++ ESP8266_SPI.h
@@ -17,13 +17,14 @@
 #include "gpio.h"
 #include "spi.h"
 
+
 #define spi_busy(spi_no) READ_PERI_REG(SPI_CMD(spi_no))&SPI_USR
 #define ESP8266_SPI_CS_LOW GPIO_OUTPUT_SET(4, 0)
 #define ESP8266_SPI_CS_HIGH GPIO_OUTPUT_SET(4, 1)
 
 void ESP8266_SPI_init_pins(void);
-void ESP8266_SPI_set_params(void);
-void ESP8266_SPI_send(uint32_t address, uint32_t data);
+void ESP8266_SPI_set_params(uint8_t addr_len, uint8_t data_len);
+void ESP8266_SPI_send(uint8_t addr_len, uint8_t data_len, uint32_t address, uint32_t data);
 void ESP8266_SPI_get(uint8_t* data);
 
 #endif
